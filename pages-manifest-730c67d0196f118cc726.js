056a7d022c50d1fe1fb2571e442492cc3a3b06b2
vincenterr <chung.menghsun@gmail.com>
2019-01-23 13:02:36 +0800

    Updates

---
+++ pages-manifest-730c67d0196f118cc726.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{133:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=pages-manifest-730c67d0196f118cc726.js.map
