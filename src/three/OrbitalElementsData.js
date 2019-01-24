b7442450619051697120c7de55e58fd666b99f96
vincenterr <chung.menghsun@gmail.com>
2019-01-24 20:06:37 +0800

    Add orbital elements of celestial bodies

---
+++ src/three/OrbitalElementsData.js
@@ -0,0 +1,242 @@
+export const sun = {
+  N: {
+    value: 0.0,
+    variation: 0.0,
+  },
+  i: {
+    value: 0.0,
+    variation: 0.0,
+  },
+  w: {
+    value: 282.9404,
+    variation: 4.70935e-5,
+  },
+  a: {
+    value: 1.0,
+    variation: 0.0,
+  },
+  e: {
+    value: 0.016709,
+    variation: -1.151e-9,
+  },
+  M: {
+    value: 356.047,
+    variation: 0.9856002585,
+  },
+}
+
+export const moon = {
+  N: {
+    value: 125.1228,
+    variation: -0.0529538083,
+  },
+  i: {
+    value: 5.1454,
+    variation: 0.0,
+  },
+  w: {
+    value: 318.0634,
+    variation: 0.1643573223,
+  },
+  a: {
+    value: 60.2666, // Earth radii
+    variation: 0.0,
+  },
+  e: {
+    value: 0.0549,
+    variation: 0.0,
+  },
+  M: {
+    value: 115.3654,
+    variation: 13.0649929509,
+  },
+}
+
+export const mercury = {
+  N: {
+    value: 48.3313,
+    variation: 3.24587e-5,
+  },
+  i: {
+    value: 7.0047,
+    variation: 5.0e-8,
+  },
+  w: {
+    value: 29.1241,
+    variation: 1.01444e-5,
+  },
+  a: {
+    value: 0.387098,
+    variation: 0.0,
+  },
+  e: {
+    value: 0.205635,
+    variation: 5.59e-10,
+  },
+  M: {
+    value: 168.6562,
+    variation: 4.0923344368,
+  },
+}
+
+export const venus = {
+  N: {
+    value: 76.6799,
+    variation: 2.4659e-5,
+  },
+  i: {
+    value: 3.3946,
+    variation: 2.75e-8,
+  },
+  w: {
+    value: 54.891,
+    variation: 1.38374e-5,
+  },
+  a: {
+    value: 0.72333,
+    variation: 0.0,
+  },
+  e: {
+    value: 0.006773,
+    variation: -1.302e-9,
+  },
+  M: {
+    value: 48.0052,
+    variation: 1.6021302244,
+  },
+}
+
+export const mars = {
+  N: {
+    value: 49.5574,
+    variation: 2.11081e-5,
+  },
+  i: {
+    value: 1.8497,
+    variation: -1.78e-8,
+  },
+  w: {
+    value: 286.5016,
+    variation: 2.92961e-5,
+  },
+  a: {
+    value: 1.523688,
+    variation: 0.0,
+  },
+  e: {
+    value: 0.093405,
+    variation: 2.516e-9,
+  },
+  M: {
+    value: 18.6021,
+    variation: 0.5240207766,
+  },
+}
+
+export const jupiter = {
+  N: {
+    value: 100.4542,
+    variation: 2.76854e-5,
+  },
+  i: {
+    value: 1.303,
+    variation: -1.557e-7,
+  },
+  w: {
+    value: 273.8777,
+    variation: 1.64505e-5,
+  },
+  a: {
+    value: 5.20256,
+    variation: 0.0,
+  },
+  e: {
+    value: 0.048498,
+    variation: 4.469e-9,
+  },
+  M: {
+    value: 19.895,
+    variation: 0.0830853001,
+  },
+}
+
+export const saturn = {
+  N: {
+    value: 113.6634,
+    variation: 2.3898e-5,
+  },
+  i: {
+    value: 2.4886,
+    variation: -1.081e-7,
+  },
+  w: {
+    value: 339.3939,
+    variation: 2.97661e-5,
+  },
+  a: {
+    value: 9.55475,
+    variation: 0.0,
+  },
+  e: {
+    value: 0.055546,
+    variation: -9.499e-9,
+  },
+  M: {
+    value: 316.967,
+    variation: 0.0334442282,
+  },
+}
+
+export const uranus = {
+  N: {
+    value: 74.0005,
+    variation: 1.3978e-5,
+  },
+  i: {
+    value: 0.7733,
+    variation: 1.9e-8,
+  },
+  w: {
+    value: 96.6612,
+    variation: 3.0565e-5,
+  },
+  a: {
+    value: 19.18171,
+    variation: -1.55e-8,
+  },
+  e: {
+    value: 0.047318,
+    variation: 7.45e-9,
+  },
+  M: {
+    vlaue: 142.5905,
+    variation: 0.011725806,
+  },
+}
+
+export const neptune = {
+  N: {
+    value: 131.7806,
+    variation: 3.0173e-5,
+  },
+  i: {
+    value: 1.77,
+    variation: -2.55e-7,
+  },
+  w: {
+    value: 272.8461,
+    variation: -6.027e-6,
+  },
+  a: {
+    value: 30.05826,
+    variation: 3.313e-8,
+  },
+  e: {
+    value: 0.008606,
+    variation: 2.15e-9,
+  },
+  M: {
+    value: 260.2471,
+    variation: 0.005995147,
+  },
+}
