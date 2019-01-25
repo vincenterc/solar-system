baf079fead23cea2c97bb5ec3102baf2239634b5
vincenterr <chung.menghsun@gmail.com>
2019-01-25 23:24:42 +0800

    Add getEccentricAnomalyInDeg function

--- src/three/Utils.js
+++ src/three/Utils.js
@@ -1,3 +1,5 @@
+import * as MathUtils from './MathUtils'
+
 // "day number" from 2000 Jan 0.0 TDT, which is the same as 1999 Dec 31.0 TDT
 export function getDayNumber(date) {
   const y = date.getFullYear()
@@ -22,4 +24,10 @@ export function getDayNumber(date) {
 
 export function getOblecl(dayNumber) {
   return 23.4393 - 3.563e-7 * dayNumber
+}
+
+export function getEccentricAnomalyInDeg(M, e) {
+  return MathUtils.wrapTo360(
+    M + e * (180 / Math.PI) * MathUtils.sind(M) * (1.0 + e * MathUtils.cosd(M))
+  )
 }
--- src/three/Utils.test.js
+++ src/three/Utils.test.js
@@ -1,4 +1,6 @@
-import { getDayNumber, getOblecl } from './Utils'
+import { getDayNumber, getOblecl, getEccentricAnomalyInDeg } from './Utils'
+import * as MathUtils from './MathUtils'
+import { sun } from './OrbitalElementsData'
 
 test('Getting DayNumber', () => {
   const date = new Date('1990-04-19T00:00:00')
@@ -10,4 +12,14 @@ test('Getting obliquity of the ecliptic', () => {
   const dayNumber = -3543
 
   expect(Math.round(getOblecl(dayNumber) * 10000) / 10000).toBe(23.4406)
+})
+
+test('Getting Eccentric Anomaly in degrees', () => {
+  const d = -3543
+  const M = MathUtils.wrapTo360(sun.M.value + sun.M.variation * d)
+  const e = sun.e.value + sun.e.variation * d
+  const E = getEccentricAnomalyInDeg(M, e)
+  expect(Math.round(M * 10000) / 10000).toBe(104.0653)
+  expect(Math.round(e * 1000000) / 1000000).toBe(0.016713)
+  expect(Math.round(E * 10000) / 10000).toBe(104.9904)
 })
