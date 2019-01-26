229a992f5ad80aab6f0fcb80bd49d44e93b920a7
vincenterr <chung.menghsun@gmail.com>
2019-01-26 22:33:11 +0800

    Modify descriptions of tests in Utils.test

--- src/three/Utils.test.js
+++ src/three/Utils.test.js
@@ -7,7 +7,7 @@ import {
 import * as MathUtils from './MathUtils'
 import { sun } from './OrbitalElementsData'
 
-test('Getting DayNumber', () => {
+test('Getting day number', () => {
   const date = new Date('1990-04-19T00:00:00')
 
   expect(getDayNumber(date)).toBe(-3543)
@@ -19,11 +19,12 @@ test('Getting obliquity of the ecliptic', () => {
   expect(MathUtils.round(getOblecl(dayNumber), 4)).toBe(23.4406)
 })
 
-test('Getting Eccentric Anomaly in degrees', () => {
+test('Getting eccentric anomaly in degrees', () => {
   const d = -3543
   const M = MathUtils.wrapTo360(sun.M.value + sun.M.variation * d)
   const e = sun.e.value + sun.e.variation * d
   const E = getEccentricAnomalyInDeg(M, e)
+
   expect(MathUtils.round(M, 4)).toBe(104.0653)
   expect(MathUtils.round(e, 6)).toBe(0.016713)
   expect(MathUtils.round(E, 4)).toBe(104.9904)
