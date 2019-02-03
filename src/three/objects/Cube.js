b330f654a743fa1500b0b5563efc83b293677a34
vincenterr <chung.menghsun@gmail.com>
2019-02-03 12:18:17 +0800

    Rewrite Cube.js to es6 classes

--- src/three/objects/Cube.js
+++ src/three/objects/Cube.js
@@ -1,15 +1,15 @@
 import * as THREE from 'three'
 
-export default scene => {
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  const mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
-
-  function update() {
-    mesh.rotation.x += 0.01
-    mesh.rotation.y += 0.01
+export default class Cube {
+  constructor(scene) {
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    this.mesh = new THREE.Mesh(geometry, material)
+    scene.add(this.mesh)
   }
 
-  return { update }
+  update() {
+    this.mesh.rotation.x += 0.01
+    this.mesh.rotation.y += 0.01
+  }
 }
